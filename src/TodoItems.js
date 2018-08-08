fe572aa56ecb49f66216ad6a8d76199272c21a77
Trace Ohrt <teohrt18@gmail.com>
2018-08-07 21:53:55 -0500

    Listing of todos implemented

---
+++ src/TodoItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class TodoItems extends React.Component {
+    createTasks(item) {
+        return <li key={item.key}>{item.text}</li>
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+        
+        return (
+            <ul className="ItemList">
+                {listItems}
+            </ul>
+        );
+    }
+}
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,46 @@
 import React from "react";
+import TodoItems from "./TodoItems";
 
 export default class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+        
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        if (this._inputElement !== "") {
+            var newTask = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+            this.setState( (prevState) => {
+                 return { items: prevState.items.concat(newTask) };
+            });
+        }
+
+        this._inputElement.value = "";
+        e.preventDefault();
+        // Makes sure the submit doesn't wipe away changes
+        
+        console.log(this.state.items);
+    }
+
     render() {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="So it shall be written..." />
+                    <form onSubmit={this.addItem}>
+                        <input  ref={(a) => this._inputElement = a} 
+                                placeholder="So it shall be written..." />
                         <button type="submit">ADD!</button>
                     </form>
                 </div>
+                <TodoItems entries={this.state.items}/>
             </div>
         );
     }
